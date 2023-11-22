bad4199522901345c9fc775734f837a6f6ae1634
inggitrhyu <146051110+inggitrhyu@users.noreply.github.com>
2023-11-22 15:21:12 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,8 @@
+def profil(nama , alamat, gender, umur, hoby):
+    print("Nama saya adalah", nama)
+    print("Saya tinggal di", alamat)
+    print("Gender saya", gender)
+    print("Umur", umur)
+    print("Hoby", hoby)
+#panggil
+profil("Inggit", "Bogor", "Perempuan", "18", "Basket")
---
+++ soal3.py
@@ -0,0 +1,6 @@
+def cetak_ganjil(n):
+    for r in range(1, n+1, 2):
+        print(r)
+
+#panggil
+cetak_ganjil(100)
